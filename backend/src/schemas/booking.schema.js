c48ab335246dc40535612222b80b991a6e1313ce
exequielcabrera98 <execabrerasarrio@gmail.com>
2024-10-16 15:22:11 -0300

    formateo de fecha

--- backend/src/schemas/booking.schema.js
+++ backend/src/schemas/booking.schema.js
@@ -7,8 +7,11 @@ export const createBookingSchema = z.object({
     treatment: z.string({
         required_error: "El tipo de tratamiento es requerido,"
     }),
-    date: z.date({
+    date: z.preprocess((arg) => {
+        // Verifica si el dato es un string y lo convierte a tipo Date
+        return typeof arg === 'string' ? new Date(arg) : arg;
+    }, z.date({
         required_error: "La fecha del tratamiento es requerida."
-    }),
+    })),
     info: z.string().optional()
 })
