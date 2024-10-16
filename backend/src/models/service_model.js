3643c39a0b97986b66b0a58908b0315ce65ff811
exequielcabrera98 <execabrerasarrio@gmail.com>
2024-10-16 00:11:35 -0300

    Ahi lo meti en el mongoose

--- backend/src/models/service_model.js
+++ backend/src/models/service_model.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
 const serviceSchema = new mongoose.Schema({
     service_name: {
@@ -11,12 +11,17 @@ const serviceSchema = new mongoose.Schema({
     },
     service_description: {
         type: String,
-        required:true
+        required: true
     },
     service_price: {
         type: Number,
         required: true
+    },
+    encargado: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     }
-})
+});
 
-export default mongoose.model('Service', serviceSchema)
+export default mongoose.model('Service', serviceSchema);
