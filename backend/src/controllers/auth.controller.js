bb530160d28fed0493b1272145c73b13b3bb6ac7
exequielcabrera98 <execabrerasarrio@gmail.com>
2024-11-11 23:26:00 -0300

    a ver ahora

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -43,7 +43,8 @@ export const register = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: true,
-            sameSite: 'None'
+            sameSite: 'None',
+            maxAge: 24 * 60 * 60 * 1000
         })
 
         res.json({
@@ -130,7 +131,8 @@ export const login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: true,
-            sameSite: 'None'
+            sameSite: 'None',
+            maxAge: 24 * 60 * 60 * 1000
         })
 
         res.json({
