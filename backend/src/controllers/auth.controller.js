a5cbfe6aa6fad298d2dafbfe2361a26805da07f7
AstridV23 <astridcarla123@gmail.com>
2025-01-07 00:53:57 -0300

    actualizacion

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -123,7 +123,11 @@ export const login = async (req, res) => {
 
         const token = await createAccessToken({id: userFound._id});
 
-        res.cookie('token', token)
+        res.cookie('token', token, {
+            httpOnly: true,
+            sameSite: 'none',
+            secure: true // Asegúrate de que tu aplicación esté usando HTTPS
+        })
 
         res.json({
             _id: userFound._id,
